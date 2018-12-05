a96734586dac5dd9d52bc721971dc0ead6539065
TEZI COSMINA <cosmina.tezi1@gmail.com>
2018-12-05 09:10:19 +0200

    Codul programului

--- TemaGhicesteNr/Program.cs
+++ TemaGhicesteNr/Program.cs
@@ -10,6 +10,34 @@ namespace TemaGhicesteNr
     {
         static void Main(string[] args)
         {
+            Random nrRandom = new Random();
+            int nrDeGhicit = nrRandom.Next(1, 100);
+            //Console.WriteLine(nrDeGhicit);
+            int nrIntrodus;
+
+            do
+            {
+                Console.WriteLine("Introduceti un nr de la 1 la 100 :");
+                nrIntrodus = int.Parse(Console.ReadLine());
+
+                if (nrIntrodus == nrDeGhicit)
+                {
+                    Console.WriteLine("Felicitari, ati ghicit numarul!");
+                }
+
+                else if (nrIntrodus < nrDeGhicit)
+                {
+                    Console.WriteLine("Numarul trebuie sa fie mai mare");
+                }
+
+                else
+                {
+                    Console.WriteLine("Numarul trebuie sa fie mai mic");
+                }
+            } while (nrIntrodus != nrDeGhicit);
+
+            Console.ReadKey(true);
         }
     }
 }
+
